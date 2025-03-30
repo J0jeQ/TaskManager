c9ddc2a00d9ad7c89a583ea652e61671f42dc899
Marcin <jojeq998@gmail.com>
2025-03-30 23:12:03 +0200

    add a breakpoints api

---
+++ controllers/taskController.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+
+let tasks = [{
+    id:1,
+    name:"posprzatac pokoj",
+    solved:"false"
+},
+{
+    id:"2",
+    name:"wyniesc smieci",
+    solved:"true"
+}];
+
+app.get('/api/tasks', (req,res)=>{
+res.send(tasks);
+});
+
+app.get('/api/tasks/:id', (req,res) =>{
+    const task = tasks.find(c=>c.id === parseInt(req.params.id));
+    if(!task) res.status(404).send("Task not exists");
+    res.send(task);
+});
+
+app.post('/api/tasks', (req,res)=>{
+const task = {
+    id: tasks.length+1,
+    name:req.body.name
+
+
+};
+tasks.push(task);
+res.send(task);
+});
+
+
+app.listen(3000, () => console.log('Hi listening on 3000 '));
